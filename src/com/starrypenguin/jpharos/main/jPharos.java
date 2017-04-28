24a003bf7102e5c2c4678d4123649cb7a408ae71
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2017-04-28 14:22:09 -0600

    Increase batch submit size

--- src/com/starrypenguin/jpharos/main/jPharos.java
+++ src/com/starrypenguin/jpharos/main/jPharos.java
@@ -80,9 +80,9 @@ final public class jPharos {
             if (ray != null) {
                 instance.executor.submit(new CastRayForDevelopedPixel(ray));
                 batchCounter++;
-                if (batchCounter == 12000) {
+                if (batchCounter == 30000) {
                     try {
-                        System.out.println("Batch " + batchNumber + "completed.  Ray submit pause . . .");
+                        System.out.println("Batch " + batchNumber + " completed.  Ray submit pause . . .");
                         Thread.sleep(30000L); // wait 30 seconds
                         batchCounter = 0;
                         System.out.println("Resuming . . .");
